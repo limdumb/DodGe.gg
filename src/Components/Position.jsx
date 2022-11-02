d75b85fa7283486d4e96bf1151e0a71e3f49eac8
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2022-11-02 21:27:08 +0900

    라인별 이미지 표현

--- src/Components/Position.jsx
+++ src/Components/Position.jsx
@@ -1,6 +1,28 @@
+import top from '../Image/top.jpg'
+import mid from '../Image/mid.jpg'
+import jungle from '../Image/jungle.jpg'
+import bottom from '../Image/bottom.jpg'
+import support from '../Image/support.jpg'
+import all from '../Image/all.jpg'
+import styled from 'styled-components'
+
+const LineImage = styled.img`
+    height: 50px;
+    width: 50px;
+    cursor: pointer;
+`
+
 
 
 export default function ChampionTear() {
     return (
+    <div>
+        <LineImage src={all}></LineImage>
+        <LineImage src={top}></LineImage>
+        <LineImage src={jungle}></LineImage>
+        <LineImage src={mid}></LineImage>
+        <LineImage src={bottom}></LineImage>
+        <LineImage src={support}></LineImage>
+    </div>
     )
 }
