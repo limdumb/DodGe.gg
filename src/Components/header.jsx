b884de99f8d32acd4fdf01d028b32bba3cf2b1d9
limdumb <dlaruddls@nate.com>
2022-11-02 22:00:55 +0900

    Header 크기 지정

---
+++ src/Components/header.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const TopHeader = styled.header`
+  background-color: black;
+  height: 48px;
+`;
+
+export default function MainHeader() {
+  return <TopHeader></TopHeader>;
+}
