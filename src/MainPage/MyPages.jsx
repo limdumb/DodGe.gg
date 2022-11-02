0c15fc127df942ed592d254ecc694cf9b7b55e60
limdumb <dlaruddls@nate.com>
2022-11-02 21:48:47 +0900

    User Information Section 지정

--- src/MainPage/MyPages.jsx
+++ src/MainPage/MyPages.jsx
@@ -1,10 +1,20 @@
-
+import MainHeader from "../Components/header";
+import "./MyPages.css"
 
 export default function MyPages() {
-
-  return(
+  return (
     <>
-      <button>hi</button>
+      <MainHeader></MainHeader>
+      <div id="Main__Container">
+        <main>
+          <section className="User__Information__Container">
+            <article className="User__Profil">하이바이</article>
+            <div className="Tier__InforMation">하이바이</div>
+          </section>
+          <div className="Information__transfer__Container"></div>
+        </main>
+        <footer></footer>
+      </div>
     </>
-  )
+  );
 }
