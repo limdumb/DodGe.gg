58a551061dc4df04f5683a365b6a5a444a038544
junpotatoes <mukdol1127@naver.com>
2022-11-02 22:17:36 +0900

    [docs, feat] 로고 및 검색창 생성

--- src/MainPage/Home.jsx
+++ src/MainPage/Home.jsx
@@ -1,7 +1,27 @@
+import MainHeader from "../Components/header";
+
 export default function Home() {
   return (
     <>
-      <button>Hello</button>
+      <MainHeader></MainHeader>
+      <div id="Main__Container">
+        <main>
+          <section>
+            <h2>Logo</h2>
+            <div className="search-box">
+              <form action="." method="post">
+                <input
+                  className="search-text"
+                  type="text"
+                  placeholder="아이디를 입력해 주세요"
+                />
+                <button className="search-btn">검색</button>
+              </form>
+            </div>
+          </section>
+        </main>
+        <footer></footer>
+      </div>
     </>
   );
 }
