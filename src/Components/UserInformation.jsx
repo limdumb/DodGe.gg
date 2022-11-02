6472bb301269446dd73ae38fc6f5248a4d0b5e33
limdumb <dlaruddls@nate.com>
2022-11-02 22:00:33 +0900

    Components 분리

---
+++ src/Components/UserInformation.jsx
@@ -0,0 +1,10 @@
+
+
+export const UserInformation = () => {
+  return (
+    <section className="User__Information__Container">
+      <article className="User__Profil">하이바이</article>
+      <div className="Tier__InforMation">하이바이</div>
+    </section>
+  );
+}
