42ed52c6b517f12485357997f68120368215ff50
limdumb <dlaruddls@nate.com>
2022-11-02 22:42:40 +0900

    Tab Component 분리

---
+++ src/Components/TypeTab.jsx
@@ -0,0 +1,11 @@
+
+
+
+export default function TypeTab() {
+  return (
+    <div className="Record__Tab__Container">
+      <div className="Tab__Title">종류별 전적</div>
+      <div className="Tab__Header">하이</div>
+    </div>
+  );
+}
