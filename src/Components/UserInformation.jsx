f1fafe1fb55355f714f76de6e35aa3c93667b1bd
limdumb <dlaruddls@nate.com>
2022-11-03 23:24:02 +0900

    User(임의)정보 지정

--- src/Components/UserInformation.jsx
+++ src/Components/UserInformation.jsx
@@ -3,7 +3,12 @@
 export const UserInformation = () => {
   return (
     <section className="User__Information__Container">
-      <article className="User__Profil">하이바이</article>
+      <article className="User__Profil">
+        <div className="Information__Contents">
+        <img className="Icon__Image" src={process.env.PUBLIC_URL + './Image/Icon.jpg'}/>
+        <span className="User__Name">늑 사 덤</span>
+        </div>
+      </article>
       <div className="Tier__InforMation">하이바이</div>
     </section>
   );
