f8da9e9f2e24f9c7849ad014f856f8f1c1af3724
limdumb <dlaruddls@nate.com>
2022-11-07 12:09:44 +0900

    [Feat] Index에 맞는 전역 상태값 설정

--- src/Components/TypeTab.jsx
+++ src/Components/TypeTab.jsx
@@ -9,8 +9,15 @@ const TabButton = styled.button`
   }
 `;
 
-export default function TypeTab() {
+export default function TypeTab({onTabChange}) {
   const [activate, setActivate] = useState(0);
+
+  const tabClickHandler = (index) => {
+    setActivate(index);
+    if(onTabChange) {
+      onTabChange(index)
+    }
+  }
 
   const tabContArr = [
     {
@@ -21,7 +28,7 @@ export default function TypeTab() {
               borderBottom: activate === 0 ? "2px solid gray" : "none",
               color: activate===0 ? "black" : "rgb(187, 187, 187)"
             }}
-            onClick={() => setActivate(0)}
+            onClick={() => tabClickHandler(0)}
           >
             전체큐
           </TabButton>
@@ -36,7 +43,7 @@ export default function TypeTab() {
               borderBottom: activate === 1 ? "2px solid gray" : "none",
               color: activate===1 ? "black" : "rgb(187, 187, 187)"
             }}
-            onClick={() => setActivate(1)}
+            onClick={() => tabClickHandler(1)}
           >
             솔로랭크
           </TabButton>
@@ -51,7 +58,7 @@ export default function TypeTab() {
               borderBottom: activate === 2 ? "2px solid gray" : "none",
               color: activate===2 ? "black" : "rgb(187, 187, 187)"
             }}
-            onClick={() => setActivate(2)}
+            onClick={() => tabClickHandler(2)}
           >
             자유랭크
           </TabButton>
@@ -66,7 +73,7 @@ export default function TypeTab() {
               borderBottom: activate===3 ? "2px solid gray" : "none",
               color: activate===3 ? "black" : "rgb(187, 187, 187)"
             }}
-            onClick={() => setActivate(3)}
+            onClick={() => tabClickHandler(3)}
           >
             일반
           </TabButton>
