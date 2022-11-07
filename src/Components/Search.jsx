418aa3cb018b3543751e09b4d999f8ce27a254e9
junpotatoes <mukdol1127@naver.com>
2022-11-07 09:04:25 +0900

    [feat] Search useState

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 
 export default function Search() {
+  const [search, setSearch] = useState("");
+  const onChange = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <div className="search-box">
       <form action="." method="">
@@ -8,6 +13,8 @@ export default function Search() {
           className="search-text"
           type="text"
           placeholder="아이디를 입력해 주세요"
+          value={search}
+          onChange={onChange}
         />
         <button className="search-btn">검색</button>
       </form>
