1f728609978982191a9a6a6b20cf7f0d7b912704
limdumb <dlaruddls@nate.com>
2022-11-07 11:59:18 +0900

    [Feat] onTabChange 함수 전달

--- src/MainPage/MyPages.jsx
+++ src/MainPage/MyPages.jsx
@@ -4,8 +4,11 @@ import { UserInformation } from "../Components/UserInformation";
 import TypeTab from "../Components/TypeTab";
 import RecordList from "../Components/RecordList";
 import Footer from "../Components/Footer";
+import { useState } from "react";
 
 export default function MyPages() {
+  const [currentTab, setCurrentTab] = useState("All__Game__Record")
+
   return (
     <>
       <MainHeader/>
@@ -16,8 +19,20 @@ export default function MyPages() {
             <span>혹시 알고 계셨나요?</span>
             <span>협곡의 전령은 바위개의 형이랍니다 응애</span>
           </div>
-          <TypeTab/>
-          <RecordList/>
+          <TypeTab 
+            onTabChange={(index) => {
+              if(index === 0) {
+                setCurrentTab("All__Game__Record")
+              } else if (index === 1) {
+                setCurrentTab("Solo__Rank__Record")
+              } else if (index === 2) {
+                setCurrentTab("Free__Rank__Record")
+              } else {
+                setCurrentTab("Normal__Game__Record")
+              }
+            }}
+          />
+          <RecordList tab={currentTab}/>
         </main>
         <Footer/>
       </div>
