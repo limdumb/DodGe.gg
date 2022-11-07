47770929e447f378a774a18805e33fc4ac379cfd
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2022-11-07 14:28:27 +0900

    MainHeader 컴포넌트 추가

---
+++ src/Components/MainHeader.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Header = styled.header`
+    background-color: black;
+    height: 48px;
+`
+
+export default function MainHeader() {
+
+    return (
+        <Header />
+    )
+}
