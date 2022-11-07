2af1f2ecb81058a154a7f1c5556f2ed8c3cab24b
junpotatoes <mukdol1127@naver.com>
2022-11-07 09:15:52 +0900

    [feat] 모든 챔피언 정보 api fetch

--- src/Components/Lotation.jsx
+++ src/Components/Lotation.jsx
@@ -1,6 +1,23 @@
 import { useState } from "react";
+import { useEffect } from "react";
 
 export default function Lotation() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(
+        `https://ddragon.leagueoflegends.com/cdn/11.12.1/data/ko_KR/champion.json`
+      );
+      const result = await res.json();
+      /* 1. 데이터를 return 하고 */
+      return result;
+    }
+    /* 2. then을 통해 response를 넘긴다. */
+    fetchData().then((res) => setData(res));
+  }, []);
+  console.log(data);
+
   return (
     <>
       <h3>금주의 로테이션</h3>
