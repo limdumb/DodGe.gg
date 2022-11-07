512e6342b8c9b52ddf39fbeeca97f758c7b9d207
limdumb <dlaruddls@nate.com>
2022-11-07 12:21:09 +0900

    [Refact] 불필요한 공백 삭제 및 컨벤션 변경

--- src/MainPage/MyPages.jsx
+++ src/MainPage/MyPages.jsx
@@ -7,34 +7,34 @@ import Footer from "../Components/Footer";
 import { useState } from "react";
 
 export default function MyPages() {
-  const [currentTab, setCurrentTab] = useState("All__Game__Record")
+  const [currentTab, setCurrentTab] = useState("All__Game__Record");
 
   return (
     <>
-      <MainHeader/>
+      <MainHeader />
       <div id="Main__Container">
         <main>
-          <UserInformation/>
+          <UserInformation />
           <div className="Information__transfer__Container">
             <span>혹시 알고 계셨나요?</span>
             <span>협곡의 전령은 바위개의 형이랍니다 응애</span>
           </div>
-          <TypeTab 
+          <TypeTab
             onTabChange={(index) => {
-              if(index === 0) {
-                setCurrentTab("All__Game__Record")
+              if (index === 0) {
+                setCurrentTab("All__Game__Record");
               } else if (index === 1) {
-                setCurrentTab("Solo__Rank__Record")
+                setCurrentTab("Solo__Rank__Record");
               } else if (index === 2) {
-                setCurrentTab("Free__Rank__Record")
+                setCurrentTab("Free__Rank__Record");
               } else {
-                setCurrentTab("Normal__Game__Record")
+                setCurrentTab("Normal__Game__Record");
               }
             }}
           />
-          <RecordList tab={currentTab}/>
+          <RecordList tab={currentTab} />
         </main>
-        <Footer/>
+        <Footer />
       </div>
     </>
   );
