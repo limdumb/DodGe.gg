8d0a63a3f8956a5968202c8b86a8c402c8ef9ebb
limdumb <dlaruddls@nate.com>
2022-11-07 12:00:06 +0900

    [Refact]Tab에 따른 Render 기능 수정

--- src/Components/RecordList.jsx
+++ src/Components/RecordList.jsx
@@ -1,10 +1,48 @@
 
 
-export default function RecordList() {
+export default function RecordList(props) {
+const getChartList = (tab) => {
+  if(tab === "All__Game__Record") {
+    return(
+      <div className="Record__List__Container">
+        <div className="List__Contents"></div>
+      </div>
+    )
+  }
+  
+  if(tab === "Solo__Rank__Record") {
+    return(
+      <>
+        <div className="Record__List__Container">
+          <div className="List__Contents"></div>
+        </div>
+        <div className="Record__List__Container">
+          <div className="List__Contents"></div>
+        </div>
+      </>
+    )
+  }
 
-  return(
-    <div className="Record__List__Container">
-      <div className="List__Contents"></div>
-    </div>
-  )
+  if(tab === "Free__Rank__Record") {
+    return(
+      <div className="Record__List__Container">
+        <div className="List__Contents"></div>
+      </div>
+    )
+  }
+
+  if(tab === "Normal__Game__Record") {
+    return(
+      <div className="Record__List__Container">
+        <div className="List__Contents"></div>
+      </div>
+    )
+  }
+}
+
+return(
+  <>
+    {getChartList(props.tab)}
+  </>  
+)
 }
