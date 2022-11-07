b764f7d00bdca99816d41570a366357e7fce22dc
junpotatoes <mukdol1127@naver.com>
2022-11-07 09:22:01 +0900

    [feat] Lotation 챔피언 정보 api fetch

--- src/Components/Lotation.jsx
+++ src/Components/Lotation.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 export default function Lotation() {
+  const [opposite, setOpposite] = useState(false);
   const [data, setData] = useState([]);
+  const [lotationKey, setLotaitionKey] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
@@ -17,6 +19,23 @@ export default function Lotation() {
     fetchData().then((res) => setData(res));
   }, []);
   console.log(data);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(
+        `https://kr.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-6d78fbda-960c-4343-9adb-1deb964770c2`
+      );
+      const result = await res.json();
+      /* 1. 데이터를 return 하고 */
+      return result;
+    }
+    /* 2. then을 통해 response를 넘긴다. */
+    fetchData().then((res) => {
+      setLotaitionKey(res.freeChampionIds);
+      setOpposite(true);
+    });
+  }, []);
+  console.log(lotationKey);
 
   return (
     <>
