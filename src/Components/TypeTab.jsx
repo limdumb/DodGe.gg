a2d41e5efff7f7a49c0a1e9fb0060ded2f3f2843
limdumb <dlaruddls@nate.com>
2022-11-07 11:44:15 +0900

    [Feat] Tab Button 구현

--- src/Components/TypeTab.jsx
+++ src/Components/TypeTab.jsx
@@ -1,10 +1,88 @@
+import { useState } from "react";
+import styled from "styled-components";
 
+const TabButton = styled.button`
+  border: none;
+  background-color: white;
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 export default function TypeTab() {
+  const [activate, setActivate] = useState(0);
+
+  const tabContArr = [
+    {
+      tabTitle: (
+        <li>
+          <TabButton
+            style={{
+              borderBottom: activate === 0 ? "2px solid gray" : "none",
+              color: activate===0 ? "black" : "rgb(187, 187, 187)"
+            }}
+            onClick={() => setActivate(0)}
+          >
+            전체큐
+          </TabButton>
+        </li>
+      ),
+    },
+    {
+      tabTitle: (
+        <li>
+          <TabButton
+            style={{
+              borderBottom: activate === 1 ? "2px solid gray" : "none",
+              color: activate===1 ? "black" : "rgb(187, 187, 187)"
+            }}
+            onClick={() => setActivate(1)}
+          >
+            솔로랭크
+          </TabButton>
+        </li>
+      ),
+    },
+    {
+      tabTitle: (
+        <li>
+          <TabButton
+            style={{
+              borderBottom: activate === 2 ? "2px solid gray" : "none",
+              color: activate===2 ? "black" : "rgb(187, 187, 187)"
+            }}
+            onClick={() => setActivate(2)}
+          >
+            자유랭크
+          </TabButton>
+        </li>
+      ),
+    },
+    {
+      tabTitle: (
+        <li>
+          <TabButton
+            style={{
+              borderBottom: activate===3 ? "2px solid gray" : "none",
+              color: activate===3 ? "black" : "rgb(187, 187, 187)"
+            }}
+            onClick={() => setActivate(3)}
+          >
+            일반
+          </TabButton>
+        </li>
+      ),
+    },
+  ];
+
   return (
     <div className="Record__Tab__Container">
       <div className="Tab__Title">종류별 전적</div>
-      <div className="Tab__Header">하이</div>
+      <div className="Tab__Type">
+        {tabContArr.map((section) => {
+          return section.tabTitle;
+        })}
+      </div>
     </div>
   );
 }
