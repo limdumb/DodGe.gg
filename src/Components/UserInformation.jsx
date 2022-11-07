1cbaa09915304808e3134e24c583d097be50c3ee
limdumb <dlaruddls@nate.com>
2022-11-07 19:58:11 +0900

    [Feat] User Tier Section 추가

--- src/Components/UserInformation.jsx
+++ src/Components/UserInformation.jsx
@@ -1,15 +1,28 @@
 
-
 export const UserInformation = () => {
   return (
     <section className="User__Information__Container">
       <article className="User__Profil">
         <div className="Information__Contents">
-        <img className="Icon__Image" src={process.env.PUBLIC_URL + './Image/Icon.jpg'}/>
-        <span className="User__Name">늑 사 덤</span>
+          <img
+            className="Icon__Image"
+            src={process.env.PUBLIC_URL + "./Image/Icon.jpg"}
+          />
+          <span className="User__Name">늑 사 덤</span>
         </div>
       </article>
-      <div className="Tier__InforMation">하이바이</div>
+      <div className="Tier__InforMation">
+          <div className="Tab__Tier__Container">
+            <img
+              className="Icon__Image"
+              src={process.env.PUBLIC_URL + "./Image/PlatinumImage.png"}
+            />
+            <div className="Tab__Tier__Contents">
+              <span className="Tab__Tier__Contents__1">Platinum 3</span>
+              <span className="Tab__Tier__Contents__2">29LP</span>
+            </div>
+          </div>
+      </div>
     </section>
   );
-}
+};
