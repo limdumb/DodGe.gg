13a70ded9d5c14a1eee7477d605b4a8188d98bea
limdumb <dlaruddls@nate.com>
2022-11-07 13:34:12 +0900

    [Refact] Styled Component로 변경

--- src/Components/RecordList.jsx
+++ src/Components/RecordList.jsx
@@ -1,46 +1,56 @@
+import styled from "styled-components";
 
+const ListContainer = styled.div`
+  width: 1200px;
+  height: 100vh;
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const RecordContents = styled.div`
+  width: 1200px;
+  height: 90px;
+  margin-top: 10px;
+  padding: 10px 18px 10px 18px;
+  background-color: rgba(59, 130, 246, 0.5);
+  border-radius: 10px;
+`;
 
 export default function RecordList(props) {
-const getChartList = (tab) => {
-  if(tab === "All__Game__Record") {
-    return(
-      <div className="Record__List__Container">
-        <div className="List__Contents">전체큐</div>
-      </div>
-    )
-  }
-  
-  if(tab === "Solo__Rank__Record") {
-    return(
-      <>
-        <div className="Record__List__Container">
-          <div className="List__Contents">자랭</div>
-          <div className="List__Contents">자랭</div>
-        </div>
-      </>
-    )
-  }
+  const getChartList = (tab) => {
+    if (tab === "All__Game__Record") {
+      return (
+        <ListContainer>
+          <RecordContents>전체큐</RecordContents>
+        </ListContainer>
+      );
+    }
 
-  if(tab === "Free__Rank__Record") {
-    return(
-      <div className="Record__List__Container">
-        <div className="List__Contents">솔랭</div>
-      </div>
-    )
-  }
+    if (tab === "Solo__Rank__Record") {
+      return (
+          <ListContainer>
+            <RecordContents>솔로랭크</RecordContents>
+          </ListContainer>
+      );
+    }
 
-  if(tab === "Normal__Game__Record") {
-    return(
-      <div className="Record__List__Container">
-        <div className="List__Contents">일반</div>
-      </div>
-    )
-  }
-}
+    if (tab === "Free__Rank__Record") {
+      return (
+        <ListContainer>
+          <RecordContents>자유랭크</RecordContents>
+        </ListContainer>
+      );
+    }
 
-return(
-  <>
-    {getChartList(props.tab)}
-  </>  
-)
+    if (tab === "Normal__Game__Record") {
+      return (
+        <ListContainer>
+          <RecordContents>일반</RecordContents>
+        </ListContainer>
+      );
+    }
+  };
+console.log(props.tab)
+  return <>{getChartList(props.tab)}</>;
 }
