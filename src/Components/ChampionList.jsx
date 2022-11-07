21c028f26f5ff287663c5c04388f16e369c77a06
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2022-11-07 14:29:31 +0900

    챔피언리스트 기본틀 작성

--- src/Components/ChampionList.jsx
+++ src/Components/ChampionList.jsx
@@ -1,7 +1,20 @@
 import Position from "./Position"
 
-export default function ChampionList () {
-    return(
-        <Position />
+export default function ChampionList() {
+    return (
+        <div className="champion_container">
+            <Position />
+            <div className="champion_list">
+                <div>
+                    <div className="search_champ">
+                        챔피언검색: <input type="text" placeholder="챔피언을 검색하세요."></input>
+                    </div>
+                    <tr>
+                        <th className="champ">챔피언</th>
+                        <th>가격</th>
+                    </tr>
+                </div>
+            </div>
+        </div>
     )
 }
