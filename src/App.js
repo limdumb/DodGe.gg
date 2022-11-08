e3ec51ed0f74b40f31cde8ab7fa257b007f38911
limdumb <dlaruddls@nate.com>
2022-11-08 23:01:25 +0900

    임시 Image 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import MyPages from "./MainPage/MyPages";
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
