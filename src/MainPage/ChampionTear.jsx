18662c3325cd36becf8d72c7869c5e3f32b5321f
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2022-11-09 15:50:04 +0900

    레이아웃 재배치

--- src/MainPage/ChampionTear.jsx
+++ src/MainPage/ChampionTear.jsx
@@ -1,7 +1,24 @@
+import MainHeader from "../Components/MainHeader";
+import "./ChampionTear.css";
+import BestChampion from "../Components/BestChampion";
+import ChampionList from "../Components/ChampionList";
+import Champion from "../Components/Champion";
 
-
-export default function ChampionTear () {
-    return (
-        <h1>hi</h1>
-    )
+export default function ChampionTear() {
+  return (
+    <>
+      <MainHeader />
+      <div id="Main__Container">
+        <BestChampion />
+        <div className="contents_container">
+          <aside className="championlist_container">
+            <ChampionList />
+          </aside>
+          <main>
+            <Champion />
+          </main>
+        </div>
+      </div>
+    </>
+  );
 }
