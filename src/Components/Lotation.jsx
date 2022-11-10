419f00c26bda39dec908bf6b86e63ace60a75ca8
junpotatoes <mukdol1127@naver.com>
2022-11-10 16:32:09 +0900

    [feat] Lotation 챔피언 이미지 렌더링

--- src/Components/Lotation.jsx
+++ src/Components/Lotation.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import "./Lotation.css";
 
 export default function Lotation() {
-  const [opposite, setOpposite] = useState(false);
+  // const [opposite, setOpposite] = useState(false);
   const [data, setData] = useState([]);
   const [lotationKey, setLotaitionKey] = useState([]);
 
@@ -23,7 +24,8 @@ export default function Lotation() {
   useEffect(() => {
     async function fetchData() {
       const res = await fetch(
-        `https://kr.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-6d78fbda-960c-4343-9adb-1deb964770c2`
+        `https://kr.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-91f5b898-9e52-4748-988b-e4aad1dafd57
+        `
       );
       const result = await res.json();
       /* 1. 데이터를 return 하고 */
@@ -32,15 +34,53 @@ export default function Lotation() {
     /* 2. then을 통해 response를 넘긴다. */
     fetchData().then((res) => {
       setLotaitionKey(res.freeChampionIds);
-      setOpposite(true);
     });
   }, []);
   console.log(lotationKey);
 
+  const champion = [];
+  const newArr = [];
+  const Url = "http://ddragon.leagueoflegends.com/cdn/12.21.1/img/champion/";
+
+  for (let data2 in data.data) {
+    champion.push(data2);
+  }
+
+  for (let i = 0; i < lotationKey.length; i++) {
+    for (let j = 0; j < champion.length; j++) {
+      if (Number(data.data[champion[j]].key) === Number(lotationKey[i])) {
+        newArr.push(data.data[champion[j]]);
+      }
+    }
+  }
+  console.log(newArr);
+
+  console.log(champion);
+
   return (
     <>
       <h3>금주의 로테이션</h3>
-      <div className="lotation"></div>
+      <div className="lotation">
+        {
+          <div className="lotation__container">
+            {data.data
+              ? newArr.map((el, idx) => {
+                  console.log(`${Url}${el.id}.png`);
+                  return (
+                    <span>
+                      <img
+                        className="lotation__img"
+                        src={`${Url}${el.id}.png`}
+                      />
+                    </span>
+                  );
+                })
+              : ""}
+          </div>
+        }
+
+        {/* { profileinfo.position.map((el)=>{ return <img src={${Url} + ${el.id}_0.jpg}></img>})} */}
+      </div>
     </>
   );
 }
