43d81edc938c42dcab7d30371b276d973af562a7
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2022-11-12 21:11:59 +0900

    챔피언리스트 탭 기능 구현

--- src/Components/Champion.jsx
+++ src/Components/Champion.jsx
@@ -1,25 +1,88 @@
-import styled from "styled-components"
+import { useState } from "react";
+import styled from "styled-components";
 
-const ChampLine = styled.span`
-    border: 1px solid red;
-    width: 130px;
-    height: 30px;
-    margin: 1px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
-
+const ChampLine = styled.button`
+  border: 1px solid red;
+  width: 130px;
+  height: 30px;
+  margin: 1px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 export default function Champion() {
-    return (
-        <div id="Champion__Container">
-            <div className="Line__Container">
-                <ChampLine>탑</ChampLine>
-                <ChampLine>정글</ChampLine>
-                <ChampLine>미드</ChampLine>
-                <ChampLine>원딜</ChampLine>
-                <ChampLine>서폿</ChampLine>
-            </div>
-        </div>
-    )
+  const [line, setLine] = useState(0);
+  const tabHandler = (idx) => {
+    setLine(idx);
+  };
+  const champContents = [
+    {
+      title: (
+        <ChampLine
+          onClick={() => {
+            tabHandler(0);
+          }}
+        >
+          탑
+        </ChampLine>
+      ),
+      content: <div style={{ border: "1px solid red" }}>hi</div>,
+    },
+    {
+      title: (
+        <ChampLine
+          onClick={() => {
+            tabHandler(1);
+          }}
+        >
+          정글
+        </ChampLine>
+      ),
+    },
+    {
+      title: (
+        <ChampLine
+          onClick={() => {
+            tabHandler(2);
+          }}
+        >
+          미드
+        </ChampLine>
+      ),
+    },
+    {
+      title: (
+        <ChampLine
+          onClick={() => {
+            tabHandler(3);
+          }}
+        >
+          원딜
+        </ChampLine>
+      ),
+    },
+    {
+      title: (
+        <ChampLine
+          onClick={() => {
+            tabHandler(4);
+          }}
+        >
+          서폿
+        </ChampLine>
+      ),
+    },
+  ];
+  return (
+    <div id="Champion__Container">
+      <div className="Line__Container">
+        {champContents.map((el, idx) => {
+          <ul>
+            <li key={idx} />
+          </ul>;
+          return el.title;
+        })}
+      </div>
+    </div>
+  );
 }
