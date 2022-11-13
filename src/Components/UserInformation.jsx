313ddd8d0798fd8052f6dd9c842f1b01e907dade
limdumb <dlaruddls@nate.com>
2022-11-13 16:01:53 +0900

    [Refact] 기존 코드 Data 정보로 리팩토링 진행

--- src/Components/UserInformation.jsx
+++ src/Components/UserInformation.jsx
@@ -1,27 +1,35 @@
+import { RankTab } from "./TypeTab";
 
-export const UserInformation = () => {
+export const UserInformation = (props) => {
+  console.log(props.lastRevisionDate);
+  const lastRevisionDate = new Date(props.lastRevisionDate);
+  console.log(lastRevisionDate);
   return (
     <section className="User__Information__Container">
       <article className="User__Profil">
         <div className="Information__Contents">
           <img
             className="Icon__Image"
-            src={process.env.PUBLIC_URL + "./Image/Icon.jpg"}
+            src={`https://ddragon.leagueoflegends.com/cdn/12.21.1/img/profileicon/${props.profilIconID}.png`}
           />
-          <span className="User__Name">늑 사 덤</span>
+          <div>
+            <span className="User__Name">{props.profileName}</span>
+            <span> 마지막 갱신일자:{lastRevisionDate.toLocaleString()} </span>
+          </div>
         </div>
       </article>
       <div className="Tier__InforMation">
-          <div className="Tab__Tier__Container">
-            <img
-              className="Icon__Image"
-              src={process.env.PUBLIC_URL + "./Image/PlatinumImage.png"}
-            />
-            <div className="Tab__Tier__Contents">
-              <span className="Tab__Tier__Contents__1">Platinum 3</span>
-              <span className="Tab__Tier__Contents__2">29LP</span>
-            </div>
+        <RankTab></RankTab>
+        <div className="Tab__Tier__Container">
+          <img
+            className="Icon__Image"
+            src={process.env.PUBLIC_URL + "./Image/PlatinumImage.png"}
+          />
+          <div className="Tab__Tier__Contents">
+            <span className="Tab__Tier__Contents__1">Platinum 3</span>
+            <span className="Tab__Tier__Contents__2">29LP</span>
           </div>
+        </div>
       </div>
     </section>
   );
