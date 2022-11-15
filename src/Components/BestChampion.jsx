e09969b2ff4ad66c841925e15cddd93f5487adc7
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2022-11-15 22:03:51 +0900

    주석 삭제

--- src/Components/BestChampion.jsx
+++ src/Components/BestChampion.jsx
@@ -3,54 +3,13 @@ import styled from "styled-components";
 import "./BestChampion.css";
 
 const Card = styled.div`
-    height: 150px;
-    width: 100px;
+    height: 300px;
+    width: 150px;
     border: solid 1px; red;
     margin: 0 0.5px;
     cursor: pointer;
 `;
-const BestChampImage = styled.img`
-  width: 100px;
-  height: 150px;
-`;
-/*export default function BestChampion() {
 
-  // State 배열로 처리함 ( 이미지를 담고있는 변수를 넣어서 Arr)
-  // 그 ARR를 useState Default값으로 넘겨준다
-
-  // 인덱스를 추적해서 바꿔주게끔 해야지 이미지를 바꿀수있게 0 === garan => 교체 다른변수로 인덱스교체
-
-
-    const [cardLocation , setCardLocation] = useState([])
-    const [card, setCard] = useState(false)
-
-    const clickHandler = () => {
-        setCard(!card)
-        console.log(card)
-    }
-
-  return(
-    
-   <div className="card_container">
-      <h3>금주의 OP 챔피언</h3>
-         <Card onClick={clickHandler}>
-          <BestChampImage src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Garen_0.jpg"/>
-         </Card>
-         <Card>
-           <BestChampImage src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Garen_0.jpg"/>
-         </Card>
-         <Card>
-          <BestChampImage src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Garen_0.jpg"/>
-         </Card>
-         <Card>
-          <BestChampImage src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Garen_0.jpg"/>
-         </Card>
-         <Card>
-           <BestChampImage src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Garen_0.jpg"/>
-         </Card>
-    </div>
-  )
-}*/
 export default function BestChampion() {
   const [card, setCard] = useState(Array(5).fill(false));
 
