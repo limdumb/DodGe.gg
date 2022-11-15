e57d9c711ed34f8d1f2ab1639b13d9d91a6a3778
Jangs <wkdwnsdlr37@gmail.com>
2022-11-16 05:05:49 +0900

    [Feat] 빌드컴포넌트 레이아웃구성

--- src/Components/ItemBulid.jsx
+++ src/Components/ItemBulid.jsx
@@ -4,7 +4,22 @@ export const ItemBulid = (()=>{
 
     return (
         <>
-        <div>123123123</div>
+            <div>
+                <hgroup>
+                    <h3>챔피언 아이템빌드</h3>
+                </hgroup>
+                <div>
+                    <ul>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                        <li>4</li>
+                    </ul>
+                </div>
+                <div><ul><li>1</li></ul></div>
+
+            </div>
+        
         </>
     )
 })
--- src/Components/SkillBulid.jsx
+++ src/Components/SkillBulid.jsx
@@ -3,7 +3,25 @@ export const SkillBulid = (()=>{
 
     return (
         <>
-        <div>스킬빌드</div>
+            <div>
+                <hgroup>
+                    <h3>챔피언 스킬빌드</h3>
+                </hgroup>
+                <div>
+                    <ul>
+                        <li>픽률</li>
+                        <li>승률</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                        <li>4</li>
+                    </ul>
+                </div>
+            </div>
         </>
     )
 })
