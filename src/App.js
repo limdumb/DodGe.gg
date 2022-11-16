09aa826c843ddafc2237bff3ab8ac4e83c21cc73
limdumb <dlaruddls@nate.com>
2022-11-16 21:44:02 +0900

    Router 연결

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
 import MyPages from "./MainPage/MyPages";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./MainPage/Home";
+import ChampionDetail from "./MainPage/ChampionDetail";
+import ChampionTier from "./MainPage/ChampionTier";
 
 function App() {
-  return
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/mypage" element={<MyPages />} />
+        <Route path="/championdetail" element={<ChampionDetail />} />
+        <Route path="/championtier" element={<ChampionTier />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
