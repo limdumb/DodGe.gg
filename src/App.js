9110416e7d9171ef01f0826810917a4d3efc2042
limdumb <99936345+limdumb@users.noreply.github.com>
2022-11-16 20:14:53 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-<<<<<<< HEAD
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import Home from "./MainPage/Home";
-import "./MainPage/Home.css";
-
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
-=======
-import logo from "./logo.svg";
 import "./App.css";
 import MyPages from "./MainPage/MyPages";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return
->>>>>>> 4dc08e155c562e3cb41ad5af1dbafa1ea22f4c55
 }
 
 export default App;
