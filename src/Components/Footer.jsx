475367fde67f0e7a33b2b80fee0d3afcf0158ee7
junpotatoes <mukdol1127@naver.com>
2022-11-20 23:08:24 +0900

    [docs] footer 아이콘추가

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,34 +1,65 @@
-import "./footer.css";
+import "./Footer.css";
 
 export default function MainFooter() {
   return (
     <footer>
+      <section className="Footer__Up__Contatiner">
+        <div>
+          <span>Get connected with us on social networks:</span>
+        </div>
+        <div className="img__Box">
+          <a href="https://github.com/limdumb/DodGe.gg">
+            <img
+              className="icon"
+              src="Image/github.png"
+              alt=""
+              width={"30px"}
+            />
+          </a>
+          <img className="icon" src="Image/velog.png" alt="" width={"30px"} />
+        </div>
+      </section>
       <div className="Footer__Container">
         <ul>
           <li>
-            <a href="https://velog.io/@limdumb/%EC%86%94%EC%B0%8D%ED%95%98%EA%B3%A0-%EC%A3%BC%EA%B4%80%EC%A0%81%EC%9D%B8-2022-FECONF-%ED%9B%84%EA%B8%B0">
+            <a
+              className="a__Tag"
+              href="https://velog.io/@limdumb/%EC%86%94%EC%B0%8D%ED%95%98%EA%B3%A0-%EC%A3%BC%EA%B4%80%EC%A0%81%EC%9D%B8-2022-FECONF-%ED%9B%84%EA%B8%B0"
+            >
               프로젝트 소개
             </a>
           </li>
         </ul>
         <ul>
           <li>
-            <a href="https://velog.io/@limdumb/%EC%86%94%EC%B0%8D%ED%95%98%EA%B3%A0-%EC%A3%BC%EA%B4%80%EC%A0%81%EC%9D%B8-2022-FECONF-%ED%9B%84%EA%B8%B0">
+            <a
+              className="a__Tag"
+              href="https://velog.io/@limdumb/%EC%86%94%EC%B0%8D%ED%95%98%EA%B3%A0-%EC%A3%BC%EA%B4%80%EC%A0%81%EC%9D%B8-2022-FECONF-%ED%9B%84%EA%B8%B0"
+            >
               limdumb
             </a>
           </li>
           <li>
-            <a href="https://velog.io/@limdumb/%EC%86%94%EC%B0%8D%ED%95%98%EA%B3%A0-%EC%A3%BC%EA%B4%80%EC%A0%81%EC%9D%B8-2022-FECONF-%ED%9B%84%EA%B8%B0">
+            <a
+              className="a__Tag"
+              href="https://velog.io/@limdumb/%EC%86%94%EC%B0%8D%ED%95%98%EA%B3%A0-%EC%A3%BC%EA%B4%80%EC%A0%81%EC%9D%B8-2022-FECONF-%ED%9B%84%EA%B8%B0"
+            >
               junpotatoes
             </a>
           </li>
           <li>
-            <a href="https://velog.io/@limdumb/%EC%86%94%EC%B0%8D%ED%95%98%EA%B3%A0-%EC%A3%BC%EA%B4%80%EC%A0%81%EC%9D%B8-2022-FECONF-%ED%9B%84%EA%B8%B0">
+            <a
+              className="a__Tag"
+              href="https://velog.io/@limdumb/%EC%86%94%EC%B0%8D%ED%95%98%EA%B3%A0-%EC%A3%BC%EA%B4%80%EC%A0%81%EC%9D%B8-2022-FECONF-%ED%9B%84%EA%B8%B0"
+            >
               MINSE97
             </a>
           </li>
           <li>
-            <a href="https://velog.io/@limdumb/%EC%86%94%EC%B0%8D%ED%95%98%EA%B3%A0-%EC%A3%BC%EA%B4%80%EC%A0%81%EC%9D%B8-2022-FECONF-%ED%9B%84%EA%B8%B0">
+            <a
+              className="a__Tag"
+              href="https://velog.io/@limdumb/%EC%86%94%EC%B0%8D%ED%95%98%EA%B3%A0-%EC%A3%BC%EA%B4%80%EC%A0%81%EC%9D%B8-2022-FECONF-%ED%9B%84%EA%B8%B0"
+            >
               jangIkIk
             </a>
           </li>
@@ -36,16 +67,25 @@ export default function MainFooter() {
 
         <ul>
           <li>
-            <a href="https://velog.io/@limdumb">limdumb Velog</a>
+            <a className="a__Tag" href="https://velog.io/@limdumb">
+              limdumb Velog
+            </a>
           </li>
           <li>
-            <a href="https://velog.io/@wnsgh156a">junpotatoes Velog</a>
+            <a className="a__Tag" href="https://velog.io/@wnsgh156a">
+              junpotatoes Velog
+            </a>
           </li>
           <li>
-            <a href="https://velog.io/@alsgood4007">MINSE97 Velog</a>
+            <a className="a__Tag" href="https://velog.io/@alsgood4007">
+              MINSE97 Velog
+            </a>
           </li>
           <li>
-            <a href="https://www.notion.so/jangss/8b2dc0fdeda44fa0acbf11329758bab3">
+            <a
+              className="a__Tag"
+              href="https://www.notion.so/jangss/8b2dc0fdeda44fa0acbf11329758bab3"
+            >
               janIkIk Notion
             </a>
           </li>
@@ -53,32 +93,50 @@ export default function MainFooter() {
 
         <ul>
           <li>
-            <a href="https://velog.io/@limdumb">limdumb Velog</a>
+            <a className="a__Tag" href="https://velog.io/@limdumb">
+              limdumb Velog
+            </a>
           </li>
           <li>
-            <a href="https://velog.io/@wnsgh156a">junpotatoes Velog</a>
+            <a className="a__Tag" href="https://velog.io/@wnsgh156a">
+              junpotatoes Velog
+            </a>
           </li>
           <li>
-            <a href="https://velog.io/@alsgood4007">MINSE97 Velog</a>
+            <a className="a__Tag" href="https://velog.io/@alsgood4007">
+              MINSE97 Velog
+            </a>
           </li>
           <li>
-            <a href="https://www.notion.so/jangss/8b2dc0fdeda44fa0acbf11329758bab3">
+            <a
+              className="a__Tag"
+              href="https://www.notion.so/jangss/8b2dc0fdeda44fa0acbf11329758bab3"
+            >
               janIkIk Notion
             </a>
           </li>
         </ul>
         <ul>
           <li>
-            <a href="https://velog.io/@limdumb">limdumb Velog</a>
+            <a className="a__Tag" href="https://velog.io/@limdumb">
+              limdumb Velog
+            </a>
           </li>
           <li>
-            <a href="https://velog.io/@wnsgh156a">junpotatoes Velog</a>
+            <a className="a__Tag" href="https://velog.io/@wnsgh156a">
+              junpotatoes Velog
+            </a>
           </li>
           <li>
-            <a href="https://velog.io/@alsgood4007">MINSE97 Velog</a>
+            <a className="a__Tag" href="https://velog.io/@alsgood4007">
+              MINSE97 Velog
+            </a>
           </li>
           <li>
-            <a href="https://www.notion.so/jangss/8b2dc0fdeda44fa0acbf11329758bab3">
+            <a
+              className="a__Tag"
+              href="https://www.notion.so/jangss/8b2dc0fdeda44fa0acbf11329758bab3"
+            >
               janIkIk Notion
             </a>
           </li>
