e4b2a8d1aeee611fd44200f8464e412f03b012eb
limdumb <dlaruddls@nate.com>
2022-11-22 21:10:08 +0900

    [Feat] Main Page Route

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import "./App.css";
 import MyPages from "./MainPage/MyPages";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ChampionDetail from "./MainPage/ChampionDetail";
+import MyPages from "./MainPage/MyPages";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<MyPages />} />
         <Route path="/mypage" element={<MyPages />} />
-        <Route path="/championdetail" element={<ChampionDetail championId={'Aatrox'}/>} />
+        <Route
+          path="/championdetail"
+          element={<ChampionDetail championId={"Aatrox"} />}
+        />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-
