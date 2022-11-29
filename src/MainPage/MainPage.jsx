11f04283f24d41e2d5fa490d436689e8efe04d5d
rlawodn46465 <rlawodn46465@naver.com>
2022-11-29 22:05:49 +0900

    [Refact] mainpage 복구

--- src/MainPage/MainPage.jsx
+++ src/MainPage/MainPage.jsx
@@ -1,9 +1,6 @@
 import { useState, useEffect } from "react";
 import React from "react";
-import OPList from "../Components/OPList/OPList";
 
 export default function MainPage() {
-  return(
-    <OPList/>
-  );
+  return;
 }
