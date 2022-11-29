cf2f2b020e275230c575ab199b4df114b6ce3d0d
limdumb <dlaruddls@nate.com>
2022-11-29 22:04:36 +0900

    충돌해결

--- src/MainPage/MainPage.jsx
+++ src/MainPage/MainPage.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import React from "react";
-import ChampionList from "../Components/ChampList/ChampionList";
 
 export default function MainPage() {
-  return(<ChampionList></ChampionList>);
+  return;
 }
