5244b0538944abd063243ca60028f13c7be08483
limdumb <dlaruddls@nate.com>
2022-11-30 16:17:43 +0900

    리뷰 참고후 코드 수정

--- src/Components/ChampList/ChampionList.jsx
+++ src/Components/ChampList/ChampionList.jsx
@@ -4,32 +4,33 @@ import { champName } from "../../API/RiotAPI";
 import { GameInfoImage } from "../MyPage/RecordList";
 import "./ChampionList.css";
 import { StyleSpan } from "../MyPage/RecordList";
-import styled from "styled-components";
 
 export default function ChampionList() {
   const [championName, setChampionName] = useState(null);
-
   useEffect(() => {
     const championData = async () => {
       const response = await champName();
       response.forEach((el) => {
-        if(el.name.length > 3){
-          el.name = el.name.substr(0, 3)+"..."
+        if (el.name.length > 3) {
+          el.name = el.name.substr(0, 3) + "...";
         }
-      })
+      });
       setChampionName(response);
     };
     championData();
   }, []);
 
-  console.log(championName)
-  const ChampionList = () => {
-    return (
-      <>
+  return (
+    <aside className="Right_Aside_Container">
+      <ul className="Champion_List_Containor">
         {championName &&
           championName.map((data, index) => {
             return (
-              <div className="Champion__List" key={index} onClick={()=> console.log('하이')}>
+              <div
+                className="Champion__List"
+                key={index}
+                onClick={() => console.log("하이")}
+              >
                 <GameInfoImage width={50} src={data.image} />
                 <div>
                   <StyleSpan fontsize={"13px"} key={index}>
@@ -39,12 +40,7 @@ export default function ChampionList() {
               </div>
             );
           })}
-      </>
-    );
-  };
-  return (
-    <aside className="Right_Aside_Container">
-      <ul className="Champion_List_Containor">{ChampionList()}</ul>
+      </ul>
     </aside>
   );
 }
