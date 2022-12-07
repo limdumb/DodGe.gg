0c2f199be5a95b7cfe359d15abaa8d5e7cc35120
limdumb <dlaruddls@nate.com>
2022-12-08 00:25:17 +0900

    [Feat] 챔피언 이름 절사 후 ... 추가

--- src/Components/ChampList/ChampionList.jsx
+++ src/Components/ChampList/ChampionList.jsx
@@ -4,19 +4,10 @@ import { champName } from "../../API/RiotAPI";
 import "./ChampionList.css";
 import { filterChampName } from "../../Function/Serch";
 
-export default function ChampionList({serchInputValue}) {
+export default function ChampionList({ serchInputValue }) {
   const [championName, setChampionName] = useState(null);
   const [champNormalName, setChampNormalName] = useState(null);
   useEffect(() => {
-    const championData = async () => {
-      const response = await champName();
-      response.forEach((el) => {
-        if (el.name.length > 3) {
-          el.name = el.name.substr(0, 3) + "...";
-        }
-      });
-      setChampionName(response);
-    };
     const championNameData = async () => {
       const championNames = {};
       const response = await champName();
@@ -26,16 +17,17 @@ export default function ChampionList({serchInputValue}) {
       setChampNormalName(response);
     };
     championNameData();
-    championData();
   }, []);
 
-  const regex = filterChampName(serchInputValue)
-  
-  const result = champNormalName&&champNormalName.filter((el, index)=>{
-    return regex.test(el.name)
-  })
+  const regex = filterChampName(serchInputValue);
 
-  console.log(result)
+  const result =
+    champNormalName &&
+    champNormalName.filter((el, index) => {
+      return regex.test(el.name);
+    });
+
+  console.log(result);
 
   return (
     <aside className="Right_Aside_Container">
@@ -55,7 +47,9 @@ export default function ChampionList({serchInputValue}) {
                 />
                 <div>
                   <span className="Champion_List_Name" key={index}>
-                    {data.name}
+                    {data.name.length > 3
+                      ? data.name.substr(0, 3) + "..."
+                      : data.name}
                   </span>
                 </div>
               </div>
