1ea6d6efbfc333c9234431c18c7f29e4c5836786
limdumb <dlaruddls@nate.com>
2022-12-12 02:27:20 +0900

    [Refact] My Page Layout 및 데이터 변경

--- src/API/RiotAPI.jsx
+++ src/API/RiotAPI.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiKey = "RGAPI-de5e97c1-6845-4fe3-a7b1-002157292e28";
+const apiKey = "";
 const apiKrBase = "https://kr.api.riotgames.com";
 const apiAsiaBase = "https://asia.api.riotgames.com";
 const UserName = "늑 사 덤";
@@ -16,7 +16,7 @@ export async function summonerData(userName) {
       name: response.data.name,
       profileIconId: response.data.profileIconId,
       summonerLevel: response.data.summonerLevel,
-      puuid: response.data.puuid
+      puuid: response.data.puuid,
     };
   } catch (error) {
     console.log(error(error));
@@ -64,6 +64,10 @@ export async function matchSummoryData(gameUuids) {
         (item) => item.teamId === 200
       );
 
+      const redTeamChampName = response.data.info.participants.filter(
+        (item) => item.championName
+      );
+
       const userChampionName = response.data.info.participants.map((el) => {
         return el.championName;
       });
@@ -101,6 +105,7 @@ export async function matchSummoryData(gameUuids) {
         redTeamSummonerName: redTeamSummonerName,
         blueTeamSummonerName: blueTeamSummonerName,
         allChampionName: userChampionName,
+        redTeamChampName: redTeamChampName,
         gameDuration: response.data.info.gameDuration,
         spellId1: summonerFilterName.summoner1Id,
         spellId2: summonerFilterName.summoner2Id,
@@ -112,7 +117,7 @@ export async function matchSummoryData(gameUuids) {
 
   let results;
   if (gameUuids) {
-    const slice = gameUuids.slice(0, 9);
+    const slice = gameUuids.slice(0, 3);
     results = await Promise.all(
       slice.map((gameUuid) => {
         return getMatchData(gameUuid);
@@ -124,11 +129,30 @@ export async function matchSummoryData(gameUuids) {
   return results;
 }
 
-export async function summonerSpell() {
+export async function summonerSpell(spellId1, spellId2) {
   try {
     const response = await axios.get(
-      "http://ddragon.leagueoflegends.com/cdn/11.3.1/data/en_US/summoner.json"
+      "http://ddragon.leagueoflegends.com/cdn/12.23.1/data/en_US/summoner.json"
     );
+    const summonerSpellName = response.data.data;
+    const twiceArr = [];
+    const result = [];
+    for (let i = 0; i < spellId1.length; i++) {
+      for (let key of Object.keys(summonerSpellName)) {
+        if (
+          summonerSpellName[key].key === spellId1[i].toString() ||
+          summonerSpellName[key].key === spellId2[i].toString()
+        ) {
+          result.push(
+            `http://ddragon.leagueoflegends.com/cdn/12.23.1/img/spell/${summonerSpellName[key].image.full}`
+          );
+        }
+      }
+    }
+    for(let i=0; i < result.length; i+= 2){
+      twiceArr.push(result.slice(i, i+2))
+    }
+    return twiceArr;
   } catch (error) {
     console.log(error(error));
   }
